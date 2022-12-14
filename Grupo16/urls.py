680333b1865507110944ada1cb9f57b18a127456
farkdev <frk.finanzas.arg@gmail.com>
2022-12-14 11:51:46 -0300

    mejorando2

--- Grupo16/urls.py
+++ Grupo16/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Grupo16.app.noticia.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -24,5 +27,7 @@ urlpatterns = [
     path('nosotros', nosotros, name="nosotros"),
     path('noticias', NoticiaListView.as_view()) ,
     path('blog', blog, name="blog")
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
