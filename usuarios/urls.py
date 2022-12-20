52591d5594c419308e5d2b4bcdf114faea21c730
farkdev <frk.finanzas.arg@gmail.com>
2022-12-20 16:07:06 -0300

    20/12

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,11 +4,14 @@ from .views import RegistroUsuario
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from Grupo16.app.noticia.views import *
 
 
 
 urlpatterns = [
-    path('registro/', RegistroUsuario.as_view(), name="registro"),
+    path('registro', RegistroUsuario.as_view(), name="registro"),
+    path('inicio', Index),
+    
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
--- usuarios/views.py
+++ usuarios/views.py
@@ -6,8 +6,10 @@ from django.urls import reverse_lazy
 class RegistroUsuario(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/registro.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('inicio')
 
 
 def login(request):
     return render(request, "login.html")
+
+
